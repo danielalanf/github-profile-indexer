178674a6e94d5ba490c4156988e358a3cbaa59d3
Daniel Alan <danielalanfaria@icloud.com>
2025-02-13 18:24:03 -0300

    Change scrapper

--- app/services/scrapper.rb
+++ app/services/scrapper.rb
@@ -1,30 +1,33 @@
+# frozen_string_literal: true
+
 require "httparty"
 
 class Scrapper
   def initialize(github_url:)
-      @github_url = github_url
-      @attributes = {}
+    @github_url = github_url
+    @attributes = {}
   end
 
   def find_attributes
     html = HTTParty.get(github_url)
     page = Nokogiri::HTML(html.body)
-    attributes[:github_user] =
-        page.xpath("//span[@itemprop=\"additionalName\"]").text.strip
-    attributes[:followers] =
-        page.xpath("//a[contains(@href, \"followers\")]/span").text.strip.to_i
-    attributes[:following] =
-        page.xpath("//a[contains(@href, \"following\")]/span").text.strip.to_i
-    attributes[:stars] =
-        page.xpath("(//a[@data-tab-item = \"stars\"]/span)[1]").text.strip.to_i
-    attributes[:last_year_contributions] =
-        page.xpath("//*[@class=\"js-yearly-contributions\"]/div/h2").text.strip.split("\n").first.to_i
+    attributes[:nickname] =
+      page.at_xpath("//span[@itemprop='additionalName']")&.text&.strip
     attributes[:image_url] =
-        page.xpath("//*[@alt=\"Avatar\"]/@src").text
+      page.at_xpath("//img[contains(@class, 'avatar-user')]/@src")&.text
+    attributes[:followers] =
+      page.at_xpath("//a[contains(@href, 'followers')]/span")&.text&.strip&.gsub(/[^\d]/, "")&.to_i
+    attributes[:following] =
+      page.at_xpath("//a[contains(@href, 'following')]/span")&.text&.strip.to_i
+    attributes[:stars] =
+      page.at_xpath("(//a[@data-tab-item = 'stars']/span)[1]")&.text&.strip.to_i
+    attributes[:last_year_contributions] =
+      page.at_xpath("//*[@class='js-yearly-contributions']//h2")&.text&.strip&.scan(/\d+/)&.first&.to_i
     attributes[:organization] =
-        page.xpath("//ul[@class=\"vcard-details\"]/li[@itemprop=\"worksFor\"]/span/div").text
+      page.at_xpath("//ul[contains(@class, 'vcard-details')]/li[@itemprop='worksFor']//span")&.text&.strip
     attributes[:location] =
-        page.xpath("//ul[@class=\"vcard-details\"]/li[@itemprop=\"homeLocation\"]/span").text
+      page.at_xpath("//ul[contains(@class, 'vcard-details')]/li[@itemprop='homeLocation']/span")&.text&.strip
+
     attributes
   end
 
--- db/migrate/20250212135345_create_users.rb
+++ db/migrate/20250212135345_create_users.rb
@@ -4,12 +4,12 @@ class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :github_url
+      t.string :github_url, null: false
       t.string :nickname, limit: 50
-      t.integer :followers
-      t.integer :following
-      t.integer :stars
-      t.integer :last_year_contributions, limit: 1
+      t.integer :followers, default: 0
+      t.integer :following, default: 0
+      t.integer :stars, default: 0
+      t.integer :last_year_contributions, default: 0
       t.string :image_url
       t.string :organization
       t.string :location
